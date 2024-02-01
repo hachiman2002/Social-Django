735e483c37c216f93124807f34ba34ab98f24703
graciany <gr4ciany@gmail.com>
2024-02-01 20:31:53 -0300

    C6: Perfil se usuario terminado (ver y editar)

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserProfileView
+from .views import UserProfileView, EditProfile
 app_name="accounts"
 
 urlpatterns = [
     path('<username>', UserProfileView.as_view(), name="profile"),
+    path('profile/edit', EditProfile, name="edit-profile"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import TemplateView, View
 from accounts.models import Profile
 from django.contrib.auth import get_user_model
 User = get_user_model()
+from django.contrib.auth.decorators import login_required
+from accounts.forms import EditProfileForm
 
 class UserProfileView(View):
     def get(self, request, username, *args, **kwargs):
@@ -14,3 +16,33 @@ class UserProfileView(View):
         }
         return render(request, 'users/detail.html', context)
     
+@login_required
+def EditProfile(request):
+    user = request.user.id
+    profile = Profile.objects.get(user__id=user)
+    user_basic_info = User.objects.get(id=user)
+
+    if request.method == 'POST':
+        form=EditProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            user_basic_info.first_name = form.cleaned_data.get('first_name')
+            user_basic_info.last_name = form.cleaned_data.get('last_name')
+
+            profile.picture = form.cleaned_data.get('picture')
+            profile.banner = form.cleaned_data.get('banner')
+            profile.location = form.cleaned_data.get('location')
+            profile.url = form.cleaned_data.get('url')
+            profile.birthday = form.cleaned_data.get('birthday')
+            profile.bio = form.cleaned_data.get('bio')
+
+            profile.save()
+            user_basic_info.save()
+            return redirect('users:profile', username=request.user.username)
+    else:
+        form=EditProfileForm(instance=profile)
+
+    context={
+        'form':form,
+    }
+
+    return render(request, 'users/edit.html', context)
