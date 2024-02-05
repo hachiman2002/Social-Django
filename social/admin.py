4b64b5c73c39dd72a2f435b87766f62b0231f8c9
graciany <gr4ciany@gmail.com>
2024-02-04 23:52:34 -0300

    C7 : Post

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from social.models import SocialPost, SocialComment, Image
 
 # Register your models here.
+admin.site.register(SocialPost)
+admin.site.register(SocialComment)
+admin.site.register(Image)
